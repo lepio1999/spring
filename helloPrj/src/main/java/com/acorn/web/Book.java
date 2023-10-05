40ae7182ea1406728d763e6e4543c1d734d3c311
lepio1999 <lepio1999@gmail.com>
2023-10-05 15:59:36 +0900

    aaa

---
+++ helloPrj/src/main/java/com/acorn/web/Book.java
@@ -0,0 +1,33 @@
+package com.acorn.web;
+
+public class Book {
+	
+	String name;
+	int price;
+	public Book(String name, int price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Book [name=" + name + ", price=" + price + "]";
+	}
+	
+	public Book() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
